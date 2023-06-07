23d9c1124f1d5e0b6ec77c0a9a312425e3ebe25e
Krishna Gautam <gtmkrishna400@gmail.com>
2023-06-07 08:21:51 +0545

    feat: function topic

---
+++ function.js
@@ -0,0 +1,36 @@
+// function nameOfFunction(name) {
+//   console.log('You are running code present inside the function');
+// }
+// nameOfFunction();
+// nameOfFunction();
+
+//Function expression
+
+// let fun = function () {
+//   console.log('this is  function expression');
+// };
+// fun();
+
+// //passing values (Argument and Parameter )
+
+// let invidations = function (name) {
+//   console.log(`Welcome to ${name} in vacation in our event`);
+// };
+
+// invidations('Krishna');
+
+//Returning the value from the function
+
+let ageCalculations = function (birthYear, curentyear = 2023) {
+  let age = curentyear - birthYear;
+  //   console.log(`Your current age is= ${age}`);
+  return age;
+};
+console.log(ageCalculations(2005));
+
+//Single line arrow function
+
+const invitation = (name) => `Welcome ${name} to the event`;
+console.log(invitation('Ram'));
+
+//Passing function as an Argument (High Order function example )
