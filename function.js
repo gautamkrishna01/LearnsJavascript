b1970bd02349013bd7d4bfc787f1502bb4b117ee
Krishna Gautam <gtmkrishna400@gmail.com>
2023-06-07 09:13:19 +0545

    feat: function topics

--- function.js
+++ function.js
@@ -1,23 +1,23 @@
-// function nameOfFunction(name) {
-//   console.log('You are running code present inside the function');
-// }
-// nameOfFunction();
-// nameOfFunction();
+function nameOfFunction(name) {
+  console.log('You are running code present inside the function');
+}
+nameOfFunction();
+nameOfFunction();
 
-//Function expression
+Function expression
 
-// let fun = function () {
-//   console.log('this is  function expression');
-// };
-// fun();
+let fun = function () {
+  console.log('this is  function expression');
+};
+fun();
 
-// //passing values (Argument and Parameter )
+//passing values (Argument and Parameter )
 
-// let invidations = function (name) {
-//   console.log(`Welcome to ${name} in vacation in our event`);
-// };
+let invidations = function (name) {
+  console.log(`Welcome to ${name} in vacation in our event`);
+};
 
-// invidations('Krishna');
+invidations('Krishna');
 
 //Returning the value from the function
 
@@ -34,3 +34,21 @@ const invitation = (name) => `Welcome ${name} to the event`;
 console.log(invitation('Ram'));
 
 //Passing function as an Argument (High Order function example )
+
+let upperCase = function (str) {
+  return str.toUpperCase();
+};
+let lowerCase = function (str) {
+  return str.toLowerCase();
+};
+let transformer = function (str, fun) {
+  return fun(str);
+};
+console.log(transformer('Helllo', upperCase));
+
+//Hosting;
+
+test();
+function test() {
+  console.log('Larning is earning');
+}
